f4c9e69b67c1d1a35b3c222ba828e4226aff4b88
Kyuubi <shadowknife11111@gmail.com>
2026-06-25 23:27:02 +0300

    revert(preloader): remove boot sequence preloader from app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,7 @@ import "@/styles/globals.css";
 import "@solana/wallet-adapter-react-ui/styles.css";
 import type { AppProps } from "next/app";
 import { Inter, Roboto, JetBrains_Mono } from "next/font/google";
-import { useState, useSyncExternalStore } from "react";
 import { SolanaWalletProvider } from "@/components/WalletProvider";
-import { Preloader } from "@/components/Preloader";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -27,22 +25,8 @@ const jetbrains = JetBrains_Mono({
 });
 
 export default function App({ Component, pageProps }: AppProps) {
-  const [showPreloader, setShowPreloader] = useState(true);
-  const mounted = useSyncExternalStore(
-    () => () => {},
-    () => true,
-    () => false
-  );
-
   return (
     <div className={`${inter.variable} ${roboto.variable} ${jetbrains.variable}`}>
-      {mounted && (
-        <Preloader
-          duration={3500}
-          isLoading={showPreloader}
-          onComplete={() => setShowPreloader(false)}
-        />
-      )}
       <SolanaWalletProvider>
         <Component {...pageProps} />
       </SolanaWalletProvider>
